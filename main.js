f4f5999e9e2b641a4c80d16cc2fc4c6004ac64c9
SULTAN-019 <sultanhyncho10@gmail.com>
2024-09-18 11:55:57 +0300

    learnibg control flow and loops

---
+++ main.js
@@ -0,0 +1,10 @@
+let trafficLightcolours = prompt("Enter you colour:");
+if (trafficLightcolours == "red"){
+    console.log("STOP");
+} else if (trafficLightcolours == "yellow"){
+    console.log("GET READY");
+}  else if (trafficLightcolours == "green"){
+    console.log("GO"); 
+} else {
+    console.log ("INVALID COLOUR INPUT");
+}
